e4e71a9b22a55f60ec8c90c216e9655115fff909
Şamil NART <samil.nart@gmail.com>
2018-03-25 15:56:55 +0300

    Add an example project

---
+++ examples/text-example/src/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { Text } from '../../..'; 
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <Text text="Hello world!" />
+            </div>
+        );
+    }
+}
+
+export default App;
