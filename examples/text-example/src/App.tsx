fdd4cda8a2a099adb64c1e296c01f0a1cb5287b2
Emre Can HACIOSMANOĞLU <emre.can.haciosmanoglu@hotmail.com>
2018-03-27 02:29:58 +0300

    Nothing special

--- examples/text-example/src/App.tsx
+++ examples/text-example/src/App.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import { Text } from '../../..'; 
+import { Line } from '../../../src/components/line-chart/LineChart';
 
 class App extends React.Component {
     render() {
         return (
             <div className="App">
                 <Text text="Hello world!" />
+                <Line data={[ 1, 2, 3, 4, 5, 6]}/>
             </div>
         );
     }
--- src/components/line-chart/LineChart.tsx
+++ src/components/line-chart/LineChart.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
 
-export class LineChart extends React.Component {
-    render() {
-        return (
-            <div className="line-chart" />
-        );
-    }
+export interface LineChart {
+    data: number[];
 }
+export const Line = ({data}: LineChart) => (
+    <svg>
+        {data.map((num, i) => <circle key={i} cx={i * 10 + 10} cy={100 - data[i]} r="1" fill="black"/>)}
+    </svg>
+);
