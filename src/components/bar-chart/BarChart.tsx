df812512b4ba16dba2a992a8738fbc3f7ab9a472
Ozan Karaali <ozan.karaali@gmail.com>
2018-03-30 23:09:17 +0300

    xy line and proper display

--- src/components/bar-chart/BarChart.tsx
+++ src/components/bar-chart/BarChart.tsx
@@ -7,17 +7,68 @@ export interface BarChartData {
     data: number[];
     
 }
+function getRandomColor(): string {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function biggestNum(data: number[]): number {
+    var bigNum = 0;
+    for (var i = 0; i < data.length; i++) {
+        if (data[i] > bigNum) {
+            bigNum = data[i];
+        }
+    }
+    return bigNum;
+}
 
 export const BarChart = ({title, subtitle, data}: BarChartData) => (
     <div className="BarChart">
+
         <div className="Title">
             <p>
                 <h1>{title}</h1>
                 <h2>{subtitle}</h2>
             </p>
         </div>
+
         <div className="ChartData">
-            {data.map((num, i) => <BarRectangle key={i} x={num} y={50 - i * 10} w={5} h={10 * i}/>)}
+            <svg width="15" height={biggestNum(data) * 10}>
+                <line 
+                    x1="0" 
+                    x2="0" 
+                    y1="0" 
+                    y2={biggestNum(data) * 10} 
+                    stroke={getRandomColor()} 
+                    strokeWidth="4" 
+                />
+
+            </svg>
+            {data.map((num, i) => 
+                <BarRectangle 
+                    key={i} 
+                    x={5} 
+                    y={(biggestNum(data) - num) * 10} 
+                    w={5} 
+                    h={10 * num}
+                    canvasH={biggestNum(data)}
+                />)}
+            <div>
+            <svg width={data.length * 15} height={15}>
+                <line 
+                    x1="0" 
+                    x2={data.length * 15}
+                    y1="0" 
+                    y2="0" 
+                    stroke={getRandomColor()} 
+                    strokeWidth="4" 
+                />
+            </svg>
+            </div>
         </div>
     </div>
 );
--- src/components/bar-chart/elements/BarRectangle.tsx
+++ src/components/bar-chart/elements/BarRectangle.tsx
@@ -10,6 +10,15 @@ export interface RectangleProps {
     color?: string;
 }
 
+function getRandomColor(): string {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
 const BarRectangle: React.SFC<RectangleProps> = (props) => {
     const x = props.x;
     const y = props.y;
@@ -17,9 +26,9 @@ const BarRectangle: React.SFC<RectangleProps> = (props) => {
     const h = props.h;
     const cW = props.canvasW || 15;
     const cH = props.canvasH || 500;
-    const color = props.color || 'black';
+    const color = props.color || getRandomColor();
     return (
-        <svg className="Rectangle" width={cW} height={cH}>
+        <svg className="Rectangle" width={cW} height={cH * 10}>
             <rect x={x} y={y} width={w} height={h} fill={color}/>
         </svg>
     );
