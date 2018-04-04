2743847da51a5c52a1cc67c0d6c2735f4558c191
Ozan Karaali <ozan.karaali@gmail.com>
2018-04-04 23:23:23 +0300

    Testing label (not working)

--- src/components/bar-chart/BarChart.tsx
+++ src/components/bar-chart/BarChart.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 export interface BarChartData {
     title?: string;
     subtitle?: string;
+    label: string[];
     data: number[];
     
 }
@@ -25,7 +26,7 @@ function biggestNum(data: number[]): number {
     return bigNum;
 }
 
-export const BarChart = ({title, subtitle, data}: BarChartData) => (
+export const BarChart = ({title, subtitle, data, label}: BarChartData) => (
     <div className="BarChart">
 
         <div className="Title">
@@ -35,20 +36,27 @@ export const BarChart = ({title, subtitle, data}: BarChartData) => (
             </p>
         </div>
 
+        <div className="Labels">
+            {label.map((num, i) => 
+                <Text key={i}>{num}</Text>
+            )
+            }
+        </div>
+
         <div className="ChartData">
             <svg width={data.length * 15} height={biggestNum(data) * 10}>
                 <line 
                     x1="0" 
                     x2="0" 
                     y1="0" 
-                    y2={biggestNum(data) * 10} 
+                    y2={data.length * 15}
                     stroke="black"
                     strokeWidth="4" 
                 />
             {data.map((num, i) => 
                 <rect
                     key={i} 
-                    x={0}
+                    x={2}
                     y={15 * i + 3.5}
                     width={10 * num}    
                     height={7.5} 
@@ -57,9 +65,9 @@ export const BarChart = ({title, subtitle, data}: BarChartData) => (
             }
                 <line 
                     x1="0" 
-                    x2={data.length * 15}
-                    y1={biggestNum(data) * 10}
-                    y2={biggestNum(data) * 10}
+                    x2={biggestNum(data) * 10}
+                    y1={data.length * 15}
+                    y2={data.length * 15}
                     stroke="black"
                     strokeWidth="4" 
                 />
