36bf8c8ac6403a9a69cc2ef836db63d7fbccc909
Ozan Karaali <ozan.karaali@gmail.com>
2018-04-07 19:56:12 +0300

    proper labels, proper scaling, proper resp-design, coloring

--- src/components/bar-chart/BarChart.tsx
+++ src/components/bar-chart/BarChart.tsx
@@ -5,6 +5,7 @@ export interface BarChartData {
     subtitle?: string;
     label: string[];
     data: number[];
+    color?: string;
     
 }
 function getRandomColor(): string {
@@ -26,34 +27,23 @@ function biggestNum(data: number[]): number {
     return bigNum;
 }
 
-function pxLen(data: string[]): number {
-    var bigNum = 0;
-    for (var i = 0; i < data.length; i++) {
-        var txt = document.getElementsByTagName('text')[i];
-        var pxlen = txt.getComputedTextLength();
-        if (pxlen > bigNum) {
-            bigNum = pxlen;
-        }
-    }
-    return bigNum;
-}
-
-export const BarChart = ({title, subtitle, data, label}: BarChartData) => (
-    <div className="BarChart">
-
-        <div className="Title">
+export const BarChart = ({title, subtitle, data, label, color}: BarChartData) => (
+    <div className="bar-chart">
+        <div className="chart-title">
             <p>
                 <h1>{title}</h1>
                 <h2>{subtitle}</h2>
             </p>
         </div>
 
-        <div className="ChartData">
-            <svg viewBox={`0 0 ${biggestNum(data) * 15} ${data.length * 17}`} >
-            {label.map((num, i) => <text id="labelText" key={i} x={0} y={i * 15 + 10} fontSize={5}>{num}</text>)}
+        <div className="chart-data">
+            <svg viewBox={`0 0 ${biggestNum(data) * 10 + 60} ${data.length * 17}`} >
+            {label.map((num, i) => <text id="label-text" key={i} x={0} y={i * 15 + 10} fontSize={5}>
+                {num.length < 16 ? num : num.substring(0, 15) + '...'}
+            </text>)}
                 <line 
-                    x1={pxLen(label) + 5} 
-                    x2={pxLen(label) + 5} 
+                    x1={50} 
+                    x2={50} 
                     y1="0" 
                     y2={data.length * 15}
                     stroke="black"
@@ -62,23 +52,24 @@ export const BarChart = ({title, subtitle, data, label}: BarChartData) => (
             {data.map((num, i) => 
                 <rect
                     key={i} 
-                    x={12}
+                    x={52}
                     y={15 * i + 3.5}
                     width={10 * num}    
                     height={7.5} 
-                    fill={getRandomColor()}
+                    fill={color || getRandomColor()}
                 />)
             }
                 <line 
-                    x1="8" 
-                    x2={biggestNum(data) * 10 + 12}
+                    x1="48" 
+                    x2={biggestNum(data) * 10 + 52}
                     y1={data.length * 15}
                     y2={data.length * 15}
                     stroke="black"
                     strokeWidth="4" 
                 />
-                <text x={10} y={data.length * 15 + 10} fontSize={5}>0</text>
-                <text x={biggestNum(data) * 10 + 10} y={data.length * 15 + 10} fontSize={5}>{biggestNum(data)}</text>
+                <text x={50} y={data.length * 15 + 10} fontSize={5}>0</text>
+                <text x={biggestNum(data) * 5 + 50} y={data.length * 15 + 10} fontSize={5}>{biggestNum(data) / 2}</text>
+                <text x={biggestNum(data) * 10 + 50} y={data.length * 15 + 10} fontSize={5}>{biggestNum(data)}</text>
             </svg>
         </div>
     </div>
