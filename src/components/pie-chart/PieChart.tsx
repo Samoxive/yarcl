3d15544dfb6a047915e7352bdd449cd94325de09
Şamil NART <samil.nart@gmail.com>
2018-04-07 22:03:05 +0300

    Get rid of var

--- src/components/pie-chart/PieChart.tsx
+++ src/components/pie-chart/PieChart.tsx
@@ -27,9 +27,9 @@ export interface PieProps {
 }
 
 function getRandomColor(): string {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
       color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
