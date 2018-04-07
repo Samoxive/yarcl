36e77015eae2baaf210a54d3cc5d63f46482252b
Ozan Karaali <ozan.karaali@gmail.com>
2018-04-07 21:56:53 +0300

    recheck

--- src/components/bar-chart/BarChart.tsx
+++ src/components/bar-chart/BarChart.tsx
@@ -9,36 +9,28 @@ export interface BarChartData {
     
 }
 function getRandomColor(): string {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
       color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
 }
 
 function biggestNum(data: number[]): number {
-    var bigNum = 0;
-    for (var i = 0; i < data.length; i++) {
-        if (data[i] > bigNum) {
-            bigNum = data[i];
-        }
-    }
-    return bigNum;
+    return Math.max.apply(Math, data);
 }
 
 export const BarChart = ({title, subtitle, data, label, color}: BarChartData) => (
     <div className="bar-chart">
-        <div className="chart-title">
-            <p>
-                <h1>{title}</h1>
-                <h2>{subtitle}</h2>
-            </p>
-        </div>
+        <p>
+            <h1>{title}</h1>
+            <h2>{subtitle}</h2>
+        </p>
 
         <div className="chart-data">
             <svg viewBox={`0 0 ${biggestNum(data) * 10 + 60} ${data.length * 17}`} >
-            {label.map((num, i) => <text id="label-text" key={i} x={0} y={i * 15 + 10} fontSize={5}>
+            {label.map((num, i) => <text key={i} x={0} y={i * 15 + 10} fontSize={5}>
                 {num.length < 16 ? num : num.substring(0, 15) + '...'}
             </text>)}
                 <line 
