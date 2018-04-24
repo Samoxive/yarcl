0ad084f1109509fb6da79e6eb2164e2862dc8a1c
Emre Can HACIOSMANOĞLU <emre.can.haciosmanoglu@hotmail.com>
2018-04-25 00:30:32 +0300

    last few touches

--- src/components/bubble-chart/BubbleChart.tsx
+++ src/components/bubble-chart/BubbleChart.tsx
@@ -8,7 +8,6 @@ export interface Subtitle {
     text: string;
 }
 export interface Label {
-    rotation: number;
     text: string;
     x: number;
     y: number;
@@ -22,14 +21,16 @@ export interface PlotLines {
 }
 export interface XAxis {
     gridLineWidth: number;
-    plotLines: PlotLines;
+    per: string;
+    plotLines: PlotLines[];
     title: Title;
     unitName: string;
 }
 
 export interface YAxis {
     gridLineWidth: number;
-    plotLines: PlotLines;
+    per: string;
+    plotLines: PlotLines[];
     title: Title;
     unitName: string;
 }
@@ -122,7 +123,7 @@ function DrawBackgroundLines(x1: number, y1: number, x2: number, y2: number) {
                     y1={i * (y2 - y1) / (a.length + 1) + y1}
                     x2={x2}
                     y2={i * (y2 - y1) / (a.length + 1) + y1}
-                    stroke-width="1"
+                    strokeWidth="1"
                     stroke="gray"
                     opacity="0.8"
                 />
@@ -134,7 +135,7 @@ function DrawBackgroundLines(x1: number, y1: number, x2: number, y2: number) {
                     y1={y1}
                     x2={i * (x2 - x1) / (b.length + 1) + x1}
                     y2={y2}
-                    stroke-width="1"
+                    strokeWidth="1"
                     stroke="gray"
                     opacity="0.8"
                 />
@@ -156,10 +157,30 @@ function PutCircles(x1: number, y1: number, x2: number, y2: number, s: Data[]) {
             <circle
                 cx={Map(s[index].x, smallX, bigX, x1, x2)} 
                 cy={Map(s[index].y, smallY, bigY, y2, y1)}
-                r={Map(s[index].z, smallZ, bigZ, 5, 50)}
+                r={Map(s[index].z, smallZ, bigZ, 3, 40)}
                 stroke="rgba(255,0,0,0.8)"
                 fill="rgba(255,0,0,0.5)"
             />
+        ))
+    );
+}
+function PutDataShortNames(x1: number, y1: number, x2: number, y2: number, s: Data[]) {
+    const bigX = BigX(s);
+    const smallX = SmallX(s);
+    const bigY = BigY(s);
+    const smallY = SmallY(s);
+
+    return (
+        s.map((d,index) => (
+            <text
+            x={Map(s[index].x, smallX, bigX, x1, x2)}
+            y={Map(s[index].y, smallY, bigY, y2, y1) + 5}
+            textAnchor="middle"
+            fill="black"
+            fontWeight="bold"
+            >
+            {s[index].shortName}
+            </text>
         ))
     );
 }
@@ -174,7 +195,7 @@ function PutYAxisInfos(x: number, y1: number, y2: number,yAxis: YAxis, s: Data[]
                 key={i}
                 x={x - 50}
                 y={(a.length + 1 - i) * (y2 - y1) / (a.length + 1) + y1}
-                stroke-width="1"
+                strokeWidth="1"
                 stroke="gray"
             >
             { "" + Math.floor(i * (bigY - smallY) / (a.length + 1) + smallY) + " " + yAxis.unitName}
@@ -191,10 +212,10 @@ function PutXAxisInfos(x1: number, x2: number, y: number, xAxis: XAxis, s: Data[
         b.map((i) =>
             <text
                 key={i}
-                text-anchor="middle"
+                textAnchor="middle"
                 x={i * (x2 - x1) / (b.length + 1) + x1}
                 y={y + 15}
-                stroke-width="1"
+                strokeWidth="1"
                 stroke="gray"
             >
             { "" + Math.floor(i * (bigX - smallX) / (b.length + 1) + smallX) + " " + xAxis.unitName}
@@ -210,90 +231,120 @@ function DrawGridLines(x1: number, y1: number, x2: number, y2: number, xAxis: XA
     const smallY = SmallY(s);
     return(
         <>
-            <line 
+            {xAxis.plotLines.map( (v) =>
+            <>
+                <line 
                 x1={x1}
                 x2={x2}
-                y1={Map(xAxis.plotLines.value, smallY, bigY, y2, y1)}
-                y2={Map(xAxis.plotLines.value, smallY, bigY, y2, y1)}
-                stroke-width={1}
-                stroke-dasharray="4, 4"
+                y1={Map(v.value, smallY, bigY, y2, y1)}
+                y2={Map(v.value, smallY, bigY, y2, y1)}
+                strokeWidth={1}
+                strokeDasharray={v.dashStyle}
                 stroke="black"
-            />
-            <text
-                text-anchor="end"
-                x={x2 - 10} 
-                y={Map(xAxis.plotLines.value, smallY, bigY, y2, y1)-10} 
-                fill="black" 
-            >
-            {
-                (xAxis.plotLines.value < bigY && xAxis.plotLines.value > smallY) ?
-                xAxis.plotLines.label.text + xAxis.plotLines.value + xAxis.unitName +"/day"  : null
-            }
-            </text>
-            <line 
-                x1={Map(yAxis.plotLines.value, smallX, bigX, x1, x2)}
-                x2={Map(yAxis.plotLines.value, smallX, bigX, x1, x2)}
-                y1={y1}
-                y2={y2}
-                stroke-width={1}
-                stroke-dasharray="4, 4"
-                stroke="black"
-            />
-            <text
-                x={Map(yAxis.plotLines.value, smallX, bigX, x1, x2) + 10} 
-                y={y1 + 20} 
-                fill="black" 
-            >
-            {
-                (yAxis.plotLines.value < bigX && yAxis.plotLines.value > smallX) ?
-                yAxis.plotLines.label.text + yAxis.plotLines.value + yAxis.unitName +"/day"  : null
-            }
-            </text>
+                />
+                <text
+                    textAnchor="end"
+                    x={x2 - 10 + v.label.x} 
+                    y={Map(v.value, smallY, bigY, y2, y1)-10 + v.label.y} 
+                    fill="black" 
+                >
+                {
+                    (v.value < bigY && v.value > smallY) ?
+                    v.label.text + 
+                    v.value +
+                    xAxis.unitName + 
+                    "/" + 
+                    xAxis.per  : null
+                }
+                </text>
+            </>
+            )}
+            {yAxis.plotLines.map( (v) =>
+            <>
+                <line 
+                    x1={Map(v.value, smallX, bigX, x1, x2)}
+                    x2={Map(v.value, smallX, bigX, x1, x2)}
+                    y1={y1}
+                    y2={y2}
+                    strokeWidth={1}
+                    strokeDasharray={v.dashStyle}
+                    stroke="black"
+                />
+                <text
+                    x={Map(v.value, smallX, bigX, x1, x2) + 10 + v.label.x} 
+                    y={y1 + 20 + v.label.y} 
+                    fill="black" 
+                >
+                {
+                    (v.value < bigX && v.value > smallX) ?
+                    v.label.text + 
+                    v.value +
+                    yAxis.unitName + 
+                    "/" + 
+                    yAxis.per  : null
+                }
+                </text>
+            </>
+            )}
         </>
     );
 }
 
 export const BubbleChart = ({title, subtitle, xAxis, yAxis, width, height, series}: BubbleChartData) => (
-    <svg width={width} height={height}>
-        <text>Coming Soon(BubbleChart)</text>
-        <rect className="background" key="1" width={width} height={height} stroke="black" fill="white"/>
-        <rect  key="2" x="100" y="50" width={width - 150} height={height - 100} stroke="black" fill="white"/>
-
-        {DrawBackgroundLines(100, 50, width - 50, height - 50)}
-        {PutCircles(100, 50, width - 50, height - 50, series)}
-        {PutYAxisInfos(100, 50, height - 50, yAxis, series)}
-        {PutXAxisInfos(100, width - 50, height - 50, xAxis, series)}
-
-        {DrawGridLines(100, 50, width - 50, height - 50, xAxis, yAxis, series)}
-
-        <text className="chart-title" key="3" textAnchor="middle" x={width / 2} y="20">
-            {title.text}
-        </text>
-        <text className="chart-subtitle" key="4" textAnchor="middle" x={width / 2} y="40">
-            {subtitle.text}
-        </text>
-        <text // Y Name
-            className="yAxis"
-            key="5"
-            textAnchor="middle" 
-            x="30" 
-            y={height / 2} 
-            fill="gray" 
-            transform={'rotate(-90 30,' + ( height / 2 ) + ' )'}
-        >
-            {yAxis.title.text}
-        </text>
-        <text // X Name
-            className="yAxis"
-            key="6"
-            textAnchor="middle" 
-            x={width /2} 
-            y={height - 10} 
-            fill="gray" 
-        >
-            {xAxis.title.text}
-        </text>
-        {/*PutDataShortNames(width, height, series)*/}
-        {/*PutDataFullNames(width, height, series)*/}
-    </svg>
+<svg width={width} height={height}>
+    <rect 
+        className="background" 
+        key="1" 
+        width={width} 
+        height={height} 
+        stroke="black" 
+        fill="white"
+    />
+    <rect  
+           key="2" 
+        x="100" 
+        y="100" 
+        width={width - 150} 
+        height={height - 150} 
+        stroke="black" 
+        fill="white" 
+        opacity="0.3"
+    />
+            
+    {DrawBackgroundLines(100, 100, width - 50, height - 50)}
+    {PutCircles(100, 100, width - 50, height - 50, series)}
+    {PutYAxisInfos(100, 100, height - 50, yAxis, series)}
+    {PutXAxisInfos(100, width - 50, height - 50, xAxis, series)}
+            
+    {DrawGridLines(100, 100, width - 50, height - 50, xAxis, yAxis, series)}
+            
+   <text className="chart-title" key="3" textAnchor="middle" x={width / 2} y="30">
+        {title.text}
+    </text>
+    <text className="chart-subtitle" key="4" textAnchor="middle" x={width / 2} y="60">
+        {subtitle.text}
+    </text>
+    <text // Y Name
+        className="yAxis"
+        key="5"
+        textAnchor="middle" 
+        x="30" 
+        y={height / 2} 
+        fill="gray" 
+        transform={'rotate(-90 30,' + ( height / 2 ) + ' )'}
+    >
+        {yAxis.title.text}
+    </text>
+    <text // X Name
+        className="yAxis"
+        key="6"
+        textAnchor="middle" 
+        x={width /2} 
+        y={height - 10} 
+        fill="gray" 
+    >
+        {xAxis.title.text}
+    </text>
+    {PutDataShortNames(100, 100, width - 50, height - 50, series)}
+ </svg>
 );
